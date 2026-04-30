0c7b34197153f30c4b921d171b949ef69bf52448
francmaki2 <francmarcushermogenes@gmail.com>
2026-05-01 00:26:57 +0800

    wit frontend(2)

--- backend/main.py
+++ backend/main.py
@@ -8,8 +8,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://frontend-production-9997.up.railway.app",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
